1e033b64dea2f16ac8117563c6a9e3f0824a7f70
Nazar <Dumi_sj18@itstepru.onmicrosoft.com>
2023-02-18 14:16:50 +0500

    Updates!

---
+++ PersonalFinance.SQL/create_db.sql
@@ -0,0 +1,53 @@
+DROP TABLE tab_operations;
+DROP TABLE tab_wallets;
+DROP TABLE tab_categories;
+DROP TABLE tab_currencies;
+
+CREATE TABLE tab_currencies
+(
+    currency_id   INT         NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    currency_name VARCHAR(10) NOT NULL,
+    currency_code VARCHAR(10) NOT NULL
+);
+
+INSERT INTO tab_currencies (currency_name, currency_code)
+VALUES ('RUR', 'no_code');
+INSERT INTO tab_currencies (currency_name, currency_code)
+VALUES ('USD', 'R01235');
+INSERT INTO tab_currencies (currency_name, currency_code)
+VALUES ('EUR', 'R01239');
+
+CREATE TABLE tab_categories
+(
+    category_id   INT  NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    category_name TEXT NOT NULL,
+    type          BOOL NOT NULL
+);
+
+CREATE TABLE tab_wallets
+(
+    wallet_id   INT           NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    wallet_name TEXT          NOT NULL,
+    currency_id INT           NOT NULL,
+    balance     DECIMAL(8, 2) NOT NULL,
+    FOREIGN KEY (currency_id) REFERENCES tab_currencies (currency_id)
+        ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+CREATE TABLE tab_operations
+(
+    operation_id   INT           NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    wallet_id      INT           NOT NULL,
+    category_id    INT           NOT NULL,
+    operation_date TEXT          NOT NULL,
+    sum            DECIMAL(8, 2) NOT NULL,
+    FOREIGN KEY (wallet_id) REFERENCES tab_wallets (wallet_id)
+        ON UPDATE NO ACTION ON DELETE NO ACTION,
+    FOREIGN KEY (category_id) REFERENCES tab_categories (category_id)
+        ON UPDATE NO ACTION ON DELETE NO ACTION
+);
+
+CREATE VIEW temp_view AS SELECT * FROM tab_operations WHERE wallet_id = 1 AND category_id = 2;
+SELECT operation_id, wallet_id, operation_date, category_name, sum
+FROM temp_view JOIN tab_categories ON tab_categories.category_id = temp_view.category_id;
+DROP VIEW temp_view;
